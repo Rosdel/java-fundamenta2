23b1aae6c81f6b95a0d8f97b1b442fb34adf9cf0
Rosdell Ouma <abonyorosdell@gmail.com>
2022-05-31 00:01:18 +0300

    getting results from our functions in arrays

--- script.js
+++ script.js
@@ -243,3 +243,16 @@
 
 /////////////////////CODING CHALLENGE About ARRAYS///////////////
 ////////////////////////////////////////////////////////////////
+ 
+const calcTip = function(bill) {  ///using expresion method
+   return bill>=50 && bill<=300 ? bill*0.15:bill*0.2; 
+}
+// const calcTip=bill=>bill>=50 && bill<=300 ? bill*0.15:bill*0.2;using arrow method
+
+const bills = [ 125, 555 , 44]
+const tips = [ calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]) ]////creating array
+console.log(bills,tips);         ///array
+
+ const total= [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]];  ///summing tips abd bill 
+ console.log(total);
+
